4a376d8ae016575854216e279d65f8daeea4c828
Ali Raeisdanaei <ali.raeisdanaei@gmail.com>
2023-02-14 13:50:05 -0500

    add new client and server files

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+ 
+# Create a connection to the server application on port 81
+tcp_socket = socket.create_connection(('localhost', 81))
+ 
+try:
+    msg = "Hi. I am a TCP client sending data to the server"
+    data = str.encode(msg)
+    tcp_socket.sendall(data)
+ 
+finally:
+    print("Closing socket")
+    tcp_socket.close()
