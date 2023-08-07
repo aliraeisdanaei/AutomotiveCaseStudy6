109d0283816f3db0af544d3048f98ad945841627
Kirill Lossev <kirill.lossev@mail.utoronto.ca>
2023-08-07 17:16:43 -0400

    Changed Android App to read from RabbitMQ

--- CollisionDetectionSample/automotive/src/main/java/com/example/collisiondetectionsample/AppSession.kt
+++ CollisionDetectionSample/automotive/src/main/java/com/example/collisiondetectionsample/AppSession.kt
@@ -4,24 +4,53 @@ import android.content.Intent
 import androidx.car.app.Screen
 import androidx.car.app.Session
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
-import kotlinx.coroutines.*
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.Delivery
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.json.JSONObject
+import kotlin.concurrent.thread
 
 class AppSession : Session() {
-    val url = "http://10.0.2.2:5000/collision"
+    val uri = "10.0.2.2"
     val queue = NetworkQueue.getInstance(carContext)
+
+    val connectionFactory = ConnectionFactory()
+    private lateinit var channel: Channel
 
     override fun onCreateScreen(intent: Intent): Screen {
         val screen = AppScreen(carContext)
+
         MainScope().launch {
             withContext(Dispatchers.IO) {
-                requestLoop(screen)
+                connectionFactory.host = uri
+                connectionFactory.port = 5672
+                val connection = connectionFactory.newConnection()
+                channel = connection.createChannel()
+
+                val q = channel.queueDeclare("obstacle_detected", false, false, false, null)
+                channel.basicConsume("obstacle_detected", true, deliverCallback(screen)) { _ -> {} }
             }
         }
         return screen
+    }
+
+    val deliverCallback: (AppScreen) -> (String, Delivery) -> Unit = { screen ->
+        { consumerTag: String, delivery: Delivery ->
+            val data = String(delivery.body, Charsets.UTF_8)
+            AppScreen.data = data
+            screen.invalidate()
+        }
+    }
+
+    fun updateLoop(screen: AppScreen) {
+        screen.invalidate()
+        Thread.sleep(5_000)
     }
 
     fun requestLoop(screen: AppScreen) {
@@ -31,7 +60,7 @@ class AppSession : Session() {
             }
         val errorListener = {error: VolleyError -> AppScreen.data = error.toString() }
         while (true) {
-            val request = JsonObjectRequest(Request.Method.GET, url, null,
+            val request = JsonObjectRequest(Request.Method.GET, uri, null,
                 respListener,
                 errorListener
             )
